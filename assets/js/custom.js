20be7f0db813ab979d9b9c47d05e0923cf8115ce
chriscalleja3 <chris.calleja.3@gov.mt>
2024-09-19 12:45:07 +0200

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -11,25 +11,33 @@ function handleTabVisibility() {
 
         var chartTab = document.querySelector('#tab-chartview');
         var mapTab = document.querySelector('#tab-mapview');
+        var embedTab = document.querySelector('#tab-embedview');
         var chartPane = document.querySelector('#chartview');
         var mapPane = document.querySelector('#mapview');
+        var embedPane = document.querySelector('#embedview');
 
         // Debug output
         console.log('Chart Tab:', chartTab);
         console.log('Map Tab:', mapTab);
+        console.log('Embed Tab:', embedTab);
         console.log('Chart Pane:', chartPane);
         console.log('Map Pane:', mapPane);
+        console.log('Embed Pane:', embedPane);
 
         if (selectedSeries === targetSeriesName) {
             if (chartTab) chartTab.style.display = 'none';
             if (mapTab) mapTab.style.display = 'none';
+            if (embedTab) embedTab.style.display = 'block';
             if (chartPane) chartPane.style.display = 'none';
             if (mapPane) mapPane.style.display = 'none';
+            if (embedPane) embedPane.style.display = 'block';
         } else {
             if (chartTab) chartTab.style.display = 'block';
             if (mapTab) mapTab.style.display = 'block';
+            if (embedTab) embedTab.style.display = 'none';
             if (chartPane) chartPane.style.display = 'block';
             if (mapPane) mapPane.style.display = 'block';
+            if (embedPane) embedPane.style.display = 'none';
         }
     } else {
         console.error('No series input is currently selected.');
