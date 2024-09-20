a4c30c9c133677fe3ecbb9639bc6d3504566704e
chriscalleja3 <chris.calleja.3@gov.mt>
2024-09-20 11:57:39 +0200

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,14 +1,30 @@
-// Sample jQuery function to display the sources based on the selected series
 $(document).ready(function() {
-    // Assuming you have a dropdown or selection for series
-    $('#series-select').on('change', function() {
-        var selectedSeries = $(this).val();
-        
-        // Hide all sources first
-        $('.source-block').hide();
+    // Define sources for each series
+    var sources = {
+        'Obesity rate by body mass index': {
+            organisation: 'Eurostat',
+            link: 'https://exampleB.com'
+        },
+        'Percentage of children aged 11,13 and 15 years who are obese or overwieght ': {
+            organisation: 'Health Behaviour in School-aged Children study (2023)',
+            link: 'https://exampleB.com'
+        }
+        // Add more series here
+    };
 
-        // Show the sources for the selected series
-        $('.source-block[data-series="' + selectedSeries + '"]').show();
+    // Loop through each label (series) and add the source information
+    $('#serieses fieldset label').each(function() {
+        var seriesName = $(this).text().trim();  // Get the text inside the label, i.e., the series name
+
+        // Check if we have a source for this series
+        if (sources[seriesName]) {
+            var sourceInfo = sources[seriesName];
+            // Create a div with the source information
+            var sourceHtml = '<div class="series-source">Source: ' +
+                sourceInfo.organisation + ' (<a href="' + sourceInfo.link + '">Link</a>)</div>';
+
+            // Append the source information to this label
+            $(this).append(sourceHtml);
+        }
     });
 });
-
