c74f5f2dd301b3910761dbc956818dc91aec0a08
chriscalleja3 <chris.calleja.3@gov.mt>
2024-09-20 11:52:20 +0200

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,1 +1,14 @@
+// Sample jQuery function to display the sources based on the selected series
+$(document).ready(function() {
+    // Assuming you have a dropdown or selection for series
+    $('#series-select').on('change', function() {
+        var selectedSeries = $(this).val();
+        
+        // Hide all sources first
+        $('.source-block').hide();
+
+        // Show the sources for the selected series
+        $('.source-block[data-series="' + selectedSeries + '"]').show();
+    });
+});
 
