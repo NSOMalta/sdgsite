051f7964c97451d79042655c82524be9a2a076ed
chriscalleja3 <chris.calleja.3@gov.mt>
2025-02-13 11:45:40 +0100

    Create chartTypeLine.js

---
+++ _includes/assets/js/view/chartTypeLine.js
@@ -0,0 +1,52 @@
+opensdg.chartTypes.line = function(info) {
+    var config = opensdg.chartTypes.base(info);
+
+    // Find the corresponding graph limit for the unit
+    var graphLimit = info.graph_limits.find(limit => limit.unit === info.unit);
+
+    var overrides = {
+        type: 'line',
+        options: {
+            scales: {
+                y: {
+                    min: graphLimit.minimum,
+                    max: graphLimit.maximum,
+                    ticks: {
+                        stepSize: graphLimit.stepSize
+                    }
+                }
+            },
+            plugins: {
+                tooltip: {
+                    mode: 'index',
+                    intersect: false,
+                },
+            },
+        },
+        plugins: [{
+            beforeDatasetsDraw: function(chart) {
+                if (chart.tooltip._active && chart.tooltip._active.length) {
+                    var activePoint = chart.tooltip._active[0],
+                        ctx = chart.ctx,
+                        x = activePoint.element.x,
+                        topY = chart.scales.y.top,
+                        bottomY = chart.scales.y.bottom;
+
+                    // draw line
+                    ctx.save();
+                    ctx.beginPath();
+                    ctx.moveTo(x, topY);
+                    ctx.lineTo(x, bottomY);
+                    ctx.lineWidth = 2;
+                    ctx.strokeStyle = '#757575';
+                    ctx.stroke();
+                    ctx.restore();
+                }
+            }
+        }],
+    };
+
+    // Add these overrides onto the normal config, and return it.
+    _.merge(config, overrides);
+    return config;
+}
