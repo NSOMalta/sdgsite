f5ed8abbbd4af5d435f1783ff32941d10d2e6e58
chriscalleja3 <chris.calleja.3@gov.mt>
2025-03-06 10:32:45 +0000

    Deploying to gh-pages from  @ 7294f8975de174798958f1e9dda36b42d467ff1e 🚀

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,30 +1,59 @@
 $(document).ready(function() {
-    // Define sources for each series
-    var sources = {
-        'Obesity rate by body mass index': {
-            organisation: 'Eurostat',
-            link: 'https://exampleB.com'
-        },
-        'Percentage of children aged 11,13 and 15 years who are obese or overwieght ': {
-            organisation: 'Health Behaviour in School-aged Children study (2023)',
-            link: 'https://exampleB.com'
+  // Get the indicator ID from the page
+  var indicatorId = $('meta[name="indicator-id"]').attr('content');
+  
+  // Define your series-to-tabs mapping in JavaScript
+  // You could also fetch this from a separate data file if you prefer
+  var seriesTabsMapping = {
+    // For indicator 1.1.1
+    '6-3-2': {
+      'Percentage of groundwater bodies exceeding the nitrate quality standard of 50mg/L': {
+        'tab_1': 'chart',
+        'tab_2': 'table',
+        'tab_3': 'hide',
+        'tab_4': 'hide'
+      },
+      'Percentage of total number of groundwater bodies with quantitative status having a good chemical status': {
+        'tab_1': 'embed',
+        'tab_2': 'table',
+        'tab_3': 'hide',
+        'tab_4': 'hide'
+      }
+    },
+    // Add more indicators as needed
+  };
+  
+  // When the series selection changes
+  $('.selection-series').on('change', function() {
+    var selectedSeries = $(this).val();
+    
+    // If we have mappings for this indicator and series
+    if (seriesTabsMapping[indicatorId] && 
+        seriesTabsMapping[indicatorId][selectedSeries]) {
+      
+      var tabsConfig = seriesTabsMapping[indicatorId][selectedSeries];
+      
+      // Apply the tab configuration for this series
+      Object.keys(tabsConfig).forEach(function(tabKey) {
+        var tabValue = tabsConfig[tabKey];
+        var tabNumber = parseInt(tabKey.replace('tab_', ''));
+        
+        if (tabValue === 'hide') {
+          // Hide this tab
+          $('.nav-tabs li:nth-child(' + tabNumber + ')').hide();
+          
+          // If the hidden tab is active, switch to the first visible tab
+          if ($('.nav-tabs li:nth-child(' + tabNumber + ')').hasClass('active')) {
+            $('.nav-tabs li:visible:first a').tab('show');
+          }
+        } else {
+          // Show this tab
+          $('.nav-tabs li:nth-child(' + tabNumber + ')').show();
+          
+          // You could also change the tab type here if needed
+          // This would depend on the specific implementation of Open SDG
         }
-        // Add more series here
-    };
-
-    // Loop through each label (series) and add the source information
-    $('#serieses fieldset label').each(function() {
-        var seriesName = $(this).text().trim();  // Get the text inside the label, i.e., the series name
-
-        // Check if we have a source for this series
-        if (sources[seriesName]) {
-            var sourceInfo = sources[seriesName];
-            // Create a div with the source information
-            var sourceHtml = '<div class="series-source">Source: ' +
-                sourceInfo.organisation + ' (<a href="' + sourceInfo.link + '">Link</a>)</div>';
-
-            // Append the source information to this label
-            $(this).append(sourceHtml);
-        }
-    });
+      });
+    }
+  });
 });
