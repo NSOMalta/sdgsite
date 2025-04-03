fc6af368090c8aa1e0b092084f8185e8b7e9da47
chriscalleja3 <chris.calleja.3@gov.mt>
2025-04-03 11:45:23 +0200

    Update chartTypeStacked.js

--- _includes/assets/js/view/chartTypeStacked.js
+++ _includes/assets/js/view/chartTypeStacked.js
@@ -1,7 +1,7 @@
-opensdg.chartTypes.bar = function (info) {
+opensdg.chartTypes.stacked = function (info) {
     var config = opensdg.chartTypes.base(info);
     var overrides = {
-        type: 'stacked',
+        type: 'bar',
     };
     if (info.stackedDisaggregation) {
         overrides.options = {
