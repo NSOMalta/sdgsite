762564f1a7d8d41615bbff1940ac0a5463d9c261
chriscalleja3 <chris.calleja.3@gov.mt>
2025-04-03 09:57:11 +0000

    Deploying to gh-pages from  @ 3aa8e7aeb87524a1292c9c69e1c658c68f3a82be 🚀

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -4046,41 +4046,6 @@ opensdg.chartTypes.base = function(info) {
     return config;
 }
 
-  opensdg.chartTypes.stacked = function (info) {
-    var config = opensdg.chartTypes.base(info);
-    var overrides = {
-        type: 'bar',
-    };
-    if (info.stackedDisaggregation) {
-        overrides.options = {
-            scales: {
-                x: { stacked: true },
-                y: { stacked: true },
-            }
-        };
-        // If we have stackedDisaggregation, we need to group datasets into stacks.
-        config.data.datasets.forEach(function (dataset) {
-            var disaggregation = $.extend({}, dataset.disaggregation);
-            // We're going to "stringify" each combination of disaggregations in order
-            // to place them in their own "stacks". To place "stacked" disaggregations
-            // into the same stack, we set them as "samestack" before stringifying.
-            // Note that the string "samestack" is completely arbitrary.
-            if (typeof disaggregation[info.stackedDisaggregation] !== 'undefined') {
-                disaggregation[info.stackedDisaggregation] = 'samestack';
-            }
-            // Use the disaggregation as a unique id for each stack.
-            dataset.stack = JSON.stringify(disaggregation);
-        });
-    }
-    // Manually set the borderWidths to 0 to avoid a weird border effect on the bars.
-    config.data.datasets.forEach(function(dataset) {
-        dataset.borderWidth = 0;
-    });
-    // Add these overrides onto the normal config, and return it.
-    _.merge(config, overrides);
-    return config;
-}
-
   opensdg.convertBinaryValue = function (value) {
     if (typeof value === 'string') {
         value = parseInt(value, 10);
@@ -4622,8 +4587,6 @@ function createIndicatorDownloadButtons(indicatorDownloads, indicatorId, el) {
     initialiseFields: initialiseFields,
     initialiseUnits: initialiseUnits,
     initialiseSerieses: initialiseSerieses,
-    updateIndicatorDataUnitStatus: updateIndicatorDataUnitStatus,
-    updateIndicatorDataSeriesStatus: updateIndicatorDataSeriesStatus,
     alterChartConfig: alterChartConfig,
     alterTableConfig: alterTableConfig,
     alterDataDisplay: alterDataDisplay,
@@ -4632,7 +4595,6 @@ function createIndicatorDownloadButtons(indicatorDownloads, indicatorId, el) {
     updateSeriesAndUnitElements: updateSeriesAndUnitElements,
     updateUnitElements: updateUnitElements,
     updateTimeSeriesAttributes: updateTimeSeriesAttributes,
-    updateObservationAttributes: updateObservationAttributes,
     updatePlot: updatePlot,
     isHighContrast: isHighContrast,
     getHeadlineColor: getHeadlineColor,
@@ -4647,8 +4609,6 @@ function createIndicatorDownloadButtons(indicatorDownloads, indicatorId, el) {
     createDownloadButton: createDownloadButton,
     createSelectionsTable: createSelectionsTable,
     sortFieldGroup: sortFieldGroup,
-    getObservationAttributeFootnoteSymbol: getObservationAttributeFootnoteSymbol,
-    getObservationAttributeText: getObservationAttributeText,
   }
 })();
 
