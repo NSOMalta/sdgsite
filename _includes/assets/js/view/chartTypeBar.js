3724777b06896db9447187c1d141980969dd35ce
chriscalleja3 <chris.calleja.3@gov.mt>
2025-04-03 11:47:19 +0200

    Update chartTypeBar.js

--- _includes/assets/js/view/chartTypeBar.js
+++ _includes/assets/js/view/chartTypeBar.js
@@ -3,19 +3,6 @@ opensdg.chartTypes.bar = function (info) {
     var overrides = {
         type: 'bar',
     };
-        
-        // If we have stackedDisaggregation, we need to group datasets into stacks.
-        config.data.datasets.forEach(function (dataset) {
-            var disaggregation = $.extend({}, dataset.disaggregation);
-            // We're going to "stringify" each combination of disaggregations in order
-            // to place them in their own "stacks". To place "stacked" disaggregations
-            // into the same stack, we set them as "samestack" before stringifying.
-            // Note that the string "samestack" is completely arbitrary.
-            
-            // Use the disaggregation as a unique id for each stack.
-            dataset.stack = JSON.stringify(disaggregation);
-        });
-    
     // Manually set the borderWidths to 0 to avoid a weird border effect on the bars.
     config.data.datasets.forEach(function(dataset) {
         dataset.borderWidth = 0;
