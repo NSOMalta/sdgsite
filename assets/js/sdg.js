85077a7aa0f3ff5d90f22bb6dfe516deb558b863
chriscalleja3 <chris.calleja.3@gov.mt>
2025-04-03 09:17:28 +0000

    Deploying to gh-pages from  @ 2e3d12ec3be9ad19d2377400b831c82787148a86 🚀

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -4037,27 +4037,6 @@ opensdg.chartTypes.base = function(info) {
     var overrides = {
         type: 'bar',
     };
-    if (info.stackedDisaggregation) {
-        overrides.options = {
-            scales: {
-                x: { stacked: true },
-                y: { stacked: true },
-            }
-        };
-        // If we have stackedDisaggregation, we need to group datasets into stacks.
-        config.data.datasets.forEach(function (dataset) {
-            var disaggregation = $.extend({}, dataset.disaggregation);
-            // We're going to "stringify" each combination of disaggregations in order
-            // to place them in their own "stacks". To place "stacked" disaggregations
-            // into the same stack, we set them as "samestack" before stringifying.
-            // Note that the string "samestack" is completely arbitrary.
-            if (typeof disaggregation[info.stackedDisaggregation] !== 'undefined') {
-                disaggregation[info.stackedDisaggregation] = 'samestack';
-            }
-            // Use the disaggregation as a unique id for each stack.
-            dataset.stack = JSON.stringify(disaggregation);
-        });
-    }
     // Manually set the borderWidths to 0 to avoid a weird border effect on the bars.
     config.data.datasets.forEach(function(dataset) {
         dataset.borderWidth = 0;
@@ -4066,6 +4045,7 @@ opensdg.chartTypes.base = function(info) {
     _.merge(config, overrides);
     return config;
 }
+
   opensdg.convertBinaryValue = function (value) {
     if (typeof value === 'string') {
         value = parseInt(value, 10);
