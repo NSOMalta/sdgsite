688427df358a1c132913de42f92312c3bff7a57d
chriscalleja3 <chris.calleja.3@gov.mt>
2025-09-25 11:15:37 +0000

    Deploying to gh-pages from  @ 656e005856f076e15c2b42c6a2f206e0f14dfd3b 🚀

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -1081,17 +1081,18 @@ event.prototype = {
   }
 };
 var accessibilitySwitcher = function () {
-
-    function getActiveContrast() {
-        const contrast = $('body').hasClass('contrast-high') ? 'high' : 'default';
+    document.addEventListener("DOMContentLoaded", (event) => {
+         const contrast = $('body').hasClass('contrast-high') ? 'high' : 'default';
          if (contrast === 'high') {
             document.getElementById('contrast-label').innerHTML = "Light Mode";
         }
         else {
             document.getElementById('contrast-label').innerHTML = "Dark Mode";
         }
-        return contrast;
-        
+        return contrast;    
+    });
+    function getActiveContrast() {
+        return $('body').hasClass('contrast-high') ? 'high' : 'default';       
     }
 
     function setHighContrast() {
